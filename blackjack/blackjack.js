b20995cf5b11841cb74b3307817ace7d12250b15
Dan Gelok <dgelok@gmail.com>
2020-06-27 17:04:02 -0500

    setup complete

---
+++ blackjack/blackjack.js
@@ -0,0 +1,17 @@
+var cards = [];
+
+// for (var i = 0; i < ?????; i++) {
+
+// }
+
+
+
+document.getElementById("btnDeal").addEventListener("click", function(){
+    //write dealer logic here
+});
+document.getElementById("btnHit").addEventListener("click", function(){
+    //write "hit" logic here
+});
+document.getElementById("btnStand").addEventListener("click", function(){
+    //write "Stand" logic here
+});
